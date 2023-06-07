468e4b8ea07cfcf54ab6b6b3fa705be29aeac4ce
focuzzed <1limbo.theorem1@gmail.com>
2023-06-07 18:58:59 +0300

    refactor: now price value is only number

--- src/view/edit-point-view.js
+++ src/view/edit-point-view.js
@@ -135,7 +135,7 @@ function createEditPointTemplate(point) {
           <span class="visually-hidden"></span>
           &euro;
         </label>
-        <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${basePrice}">
+        <input class="event__input  event__input--price" id="event-price-1" type="number" name="event-price" value="${basePrice}">
       </div>
 
       <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
